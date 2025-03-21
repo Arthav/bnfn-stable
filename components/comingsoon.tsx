c1b961644a5236fb38317deb0bac213b2ed37614
Christian Bonafena <christianbonafena7@gmail.com>
2025-03-21 13:50:19 +0700

    fix apos

--- components/comingsoon.tsx
+++ components/comingsoon.tsx
@@ -5,7 +5,7 @@ const ComingSoon: React.FC = () => (
     <div className="text-center">
       <h1 className="text-5xl font-bold mb-4"> Coming Soon </h1>
       <p className="text-lg mb-8">
-        We're working hard to bring you this feature.
+        We&apos;re working hard to bring you this feature.
       </p>
       <div className="inline-flex space-x-2">
         <span className="h-2 w-2 bg-white rounded-full animate-ping"></span>
