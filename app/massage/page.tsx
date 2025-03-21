25a1670446a8a4593662fbbea496f584c6a92acb
Christian Bonafena <christianbonafena7@gmail.com>
2025-03-21 14:50:16 +0700

    update add more good style

--- app/massage/page.tsx
+++ app/massage/page.tsx
@@ -7,23 +7,63 @@ import ComingSoon from "@/components/comingsoon";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import TemplatePage from "@/components/massage/TemplatePage";
+import {
+  FaSpa,
+  FaCalendarAlt,
+  FaConciergeBell,
+  FaPlusSquare,
+  FaMoneyBillAlt,
+  FaChartBar,
+  FaFileAlt,
+} from "react-icons/fa";
 
 export default function MassageShiftPage() {
   const [activeTab, setActiveTab] = useState("massage-shift");
   const [services, setServices] = useState<Services[]>([]);
-  
+
   const tabs = [
     {
       key: "massage-shift",
       title: "Massage Shift",
+      icon: <FaSpa />,
       component: <MassageShift services={services} />,
     },
-    { key: "booking", title: "Booking", component: <ComingSoon /> },
-    { key: "manage-service", title: "Manage Service", component: <ComingSoon /> },
-    { key: "manage-addons", title: "Manage Add-ons", component: <ComingSoon /> },
-    { key: "transaction", title: "Transaction", component: <ComingSoon /> },
-    { key: "report", title: "Report", component: <ComingSoon /> },
-    { key: "template", title: "Template", component: <TemplatePage /> },
+    {
+      key: "booking",
+      title: "Booking",
+      icon: <FaCalendarAlt />,
+      component: <ComingSoon />,
+    },
+    {
+      key: "manage-service",
+      title: "Manage Service",
+      icon: <FaConciergeBell />,
+      component: <ComingSoon />,
+    },
+    {
+      key: "manage-addons",
+      title: "Manage Add-ons",
+      icon: <FaPlusSquare />,
+      component: <ComingSoon />,
+    },
+    {
+      key: "transaction",
+      title: "Transaction",
+      icon: <FaMoneyBillAlt />,
+      component: <ComingSoon />,
+    },
+    {
+      key: "report",
+      title: "Report",
+      icon: <FaChartBar />,
+      component: <ComingSoon />,
+    },
+    {
+      key: "template",
+      title: "Template",
+      icon: <FaFileAlt />,
+      component: <TemplatePage />,
+    },
   ];
 
   useEffect(() => {
@@ -35,20 +75,25 @@ export default function MassageShiftPage() {
 
   return (
     <div className="container mx-auto max-w-7xl px-6">
-      <div className="pb-5 mb-4 mb-2">
-        <nav className="-mb-px flex justify-center space-x-6">
+      <div className="pb-5 mb-4">
+        <p className="flex-1 flex flex-col items-center justify-center text-center px-4 py-2 font-bold text-white">
+          ADMIN MENU PANEL
+        </p>
+        <nav className="-mb-px flex gap-x-4">
           <ToastContainer />
-          {tabs.map(({ key, title }) => (
+
+          {tabs.map(({ key, title, icon }) => (
             <button
               key={key}
               onClick={() => setActiveTab(key)}
-              className={`px-4 py-2 font-medium transition-colors duration-200 ${
+              className={`flex-1 flex flex-col items-center justify-center text-center px-4 py-2 font-medium transition-colors duration-200 ${
                 activeTab === key
                   ? "bg-lime-500 border-b border-orange-500 text-white-900 font-bold shadow-md shadow-purple-400/50"
                   : "border-b-2 border-transparent text-white-400 hover:text-purple-400"
               }`}
             >
-              {title}
+              {icon}
+              <span className="mt-1">{title}</span>
             </button>
           ))}
         </nav>
