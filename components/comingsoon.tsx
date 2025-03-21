18b3f1faa5d3a38094a311883275d98003dd2a09
Christian Bonafena <christianbonafena7@gmail.com>
2025-03-21 13:50:39 +0700

    add rocker

--- components/comingsoon.tsx
+++ components/comingsoon.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const ComingSoon: React.FC = () => (
   <div className="flex items-center justify-center h-screen bg-gray-900 text-white px-4">
     <div className="text-center">
-      <h1 className="text-5xl font-bold mb-4"> Coming Soon </h1>
+      <h1 className="text-5xl font-bold mb-4">🚀 Coming Soon 🚀</h1>
       <p className="text-lg mb-8">
         We&apos;re working hard to bring you this feature.
       </p>
